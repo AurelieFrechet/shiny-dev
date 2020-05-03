08998eea2b58e2ee07f9ac24fd350585d08d7c9f
agailloty <mr.clerisaxel@hotmail.fr>
2020-05-04 00:33:43 +0200

    Making map full screen and shiny component above the leaflet map

--- Covid19/R/funcs/utilityFuncs.R
+++ Covid19/R/funcs/utilityFuncs.R
@@ -10,18 +10,36 @@ dateFormat <- function(minus = 0) {
 
 retrieveTodayData <- function(delay = 1) {
   basePath <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
-testPath <- paste0(basePath, dateFormat(), ".csv") 
-if (url.exists(testPath)) {
-  todayPath <- testPath
-} else {
-  todayPath = paste0(basePath, dateFormat(delay), ".csv")
-}
-print(todayPath)
-readr::read_csv(todayPath)
+  testPath <- paste0(basePath, dateFormat(), ".csv") 
+  while (! url.exists(testPath)) {
+    testPath = paste0(basePath, dateFormat(delay), ".csv")
+    delay = delay + 1
+  } 
+  todayPath = testPath
+  print(todayPath)
+  readr::read_csv(todayPath)
 }
 
-# Find continent names from Country
-# Read data taken from https://datahub.io/JohnSnowLabs/country-and-continent-codes-list#data
+convertDataTS <- function(df, from = 5){
+  nbCols <- ncol(df)
+  df %>%
+    pivot_longer(cols = from:nbCols)
+}
 
-#continent <- read_csv("covid/data/continent.csv", na = "empty")
+
+
+# Find the continent of each Country_Region and save it as CountryContinent.csv
+# library(countrycode)
+# todayData %>% 
+#   group_by(Country_Region) %>% 
+#   summarize(totalConfirmed = sum(Confirmed),
+#             totalDeath = sum(Deaths),
+#             totalRecovered = sum(Recovered),
+#             totalActive = sum(Active)) %>%
+#   mutate(continent = countrycode(sourcevar = Country_Region,
+#                                  origin = "country.name", 
+#                                  destination = "continent"))
+# 
+# statsCountry[, c("Country_Region", "continent")] %>%
+#   write.csv(, file = "data/CountryContinent.csv", row.names = FALSE)
 
--- Covid19/R/ui/todayUI.R
+++ Covid19/R/ui/todayUI.R
@@ -1,20 +1,35 @@
 todayUI <- tabsetPanel(
   tabPanel(
     "Cases world map",
-    sidebarLayout(
-      sidebarPanel(
-        width = 3,
+    fluidRow(
+      h3(paste("The latest update of the data is on the", 
+              todayData$Last_Update[1]))
+    ),
+      absolutePanel(
+        bottom = 20, right = 20, width = 300,
+        draggable = TRUE,
         selectInput("dataSource", label = "Which Data", 
                     choices = c("Confirmed", "Deaths", "Recovered"),
                     selected = "Confirmed"),
         uiOutput("selectRegions"),
-        actionButton("clearMap", "Clear")
+        actionButton("clearMap", "Clear"),
+        style = "opacity: 0.88; z-index:500;"
       ),
-      mainPanel(
-        width = 9,
-        leafletOutput("worldMap", width = 1200, height=800)
-      )
-    )
+      leafletOutput("worldMap", width = 1200, height=800)
+    # sidebarLayout(
+    #   sidebarPanel(
+    #     width = 3,
+    #     selectInput("dataSource", label = "Which Data", 
+    #                 choices = c("Confirmed", "Deaths", "Recovered"),
+    #                 selected = "Confirmed"),
+    #     uiOutput("selectRegions"),
+    #     actionButton("clearMap", "Clear")
+    #   ),
+    #   mainPanel(
+    #     width = 9,
+    #     leafletOutput("worldMap", width = 1200, height=800)
+    #   )
+    # )
   ),
   tabPanel("Ex",
     fluidRow(column(6, 
